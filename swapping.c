8537dddb4cf63f07be83eee925749f2a73054189
Samrat12366 <64467874+Samrat12366@users.noreply.github.com>
2020-04-28 15:22:49 +0530

    created  swapping.c
    This  file  swaps two  numbers  without  using  temporary or  any  third  variable

---
+++ swapping.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+#include<conio.h>
+main()    
+{    
+int a=10, b=20;  //initializing two numbers        
+a=a+b;//a=30 (10+20)    
+b=a-b;//b=10 (30-20)    
+a=a-b;//a=20 (30-10)    
+printf("\nAfter swapping a=%d b=%d",a,b);    
+}   
